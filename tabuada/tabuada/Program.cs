133a6a0ab5510a10e5b146dbe84465747e080e99
andre saavedra <andre.saavedra@hotmail.com>
2020-12-30 00:17:22 +0000

    Tabuada

--- tabuada/tabuada/Program.cs
+++ tabuada/tabuada/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace tabuada
+namespace Tabuada
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Introduza um número");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\nTabuada do {n}\n");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} x {n} = {i * n}");
+            }
+            Console.ReadKey();
         }
     }
 }
